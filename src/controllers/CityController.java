3b98989977ada679b211fc72b5a0fe0278545360
Chris-AM <c.aranguizm@outlook.com>
2021-03-02 20:02:30 -0300

    método registrar ciudad creado

--- src/controllers/CityController.java
+++ src/controllers/CityController.java
@@ -2,10 +2,16 @@
 package controllers;
 
 import model.City;
+import model.CityDao;
 public class CityController {
     
     public boolean registerCity(City city){
         
-        return false;
+        boolean register = false;
+        
+        CityDao cityDao = new CityDao();
+        register = cityDao.insertCity(city);
+        
+        return register;
     }
 }
